beb5822ea5d3cf3610c976feb592052a2ecf2518
hajtos <hajtos2@gmail.com>
2019-01-08 23:30:25 +0100

    Signals according to word document

--- ichimoku_strategy.py
+++ ichimoku_strategy.py
@@ -27,14 +27,31 @@ class IchimokuStrategy(Strategy):
                 times += 1
             i += 1
 
-    def get_signal(self, index):
-        graph4h = self.graph
-        graph24h = self.graphs[0]
-        index_24h = graph24h.get_my_index_for(index, graph4h.timeframe)
-        if graph4h.tenkan_sen(index) > graph4h.kijun_sen(index):
-            if graph4h.close(index) > graph4h.close(index - 26) and graph24h.kijun_sen(index_24h) < graph24h.close(index_24h):
-                return (1, 1, 0)
-        return (0, 0, 0)
+    def get_signal(self, index, direction):
+        # signal 1 kijun-tenkan cross, kumo adjusting required
+        signal1_present = (self.graph.tenkan_sen(index) - self.graph.kijun_sen(index)) * direction > 0 \
+            and (self.graph.tenkan_sen(index) - self.graph.senkou_span_A(index - 26)) * direction > 0 \
+            and (self.graph.tenkan_sen(index) - self.graph.senkou_span_B(index - 26)) * direction > 0
+        signal1_age = 1
+        while (self.graph.tenkan_sen(index - signal1_age) - self.graph.kijun_sen(index - signal1_age)) * direction > 0:
+            signal1_age += 1
+        # signal 2 kumo cross
+        signal2_present = (self.graph.senkou_span_A(index) - self.graph.senkou_span_B(index)) * direction > 0
+        signal2_age = 1
+        while (self.graph.senkou_span_A(index - signal2_age) - self.graph.senkou_span_B(index - signal2_age)) * direction > 0:
+            signal2_age += 1
+        # signal 3 graph crossing kumo
+        signal3_present = (self.graph.close(index) - self.graph.senkou_span_A(index)) * direction > 0 \
+            and (self.graph.close(index) - self.graph.senkou_span_B(index)) * direction > 0
+        signal3_age = 1
+        while (self.graph.close(index - signal3_age) - self.graph.senkou_span_A(index - signal3_age)) * direction > 0 \
+                and (self.graph.close(index - signal3_age) - self.graph.senkou_span_B(index - signal3_age)) * direction > 0:
+            signal3_age += 1
+        signals = [(t, age) for t, pres, age in [(1, signal1_present, signal1_age), (2, signal2_present, signal2_age),
+                                                 (3, signal3_present, signal3_age)] if pres]
+        if signals:
+            return min(signals, key=lambda x: x[1])
+        return (0, 0)
 
     def stoploss(self, index, signal, signal_type):
         if signal_type == 1 and signal == 1:
@@ -51,14 +68,18 @@ class IchimokuStrategy(Strategy):
     def check_for_entry(self, index):
         if index <= self.last_close:
             return False
-        signal, signal_type, age = self.get_signal(index)
-        if not signal:
+        direction = 1 if self.graph.tenkan_sen(index) > self.graph.tenkan_sen(index - 1) else -1
+        if direction * (self.graph.close(index) - self.graph.close(index - 26)) < 0:
             return False
-        stoploss = self.stoploss(index, signal, signal_type)
-        takeprofit = self.takeprofit(index, signal, signal_type)
+        signal_type, age = self.get_signal(index, direction)
+        anti_signal, _ = self.get_signal(index, -direction)
+        if not signal_type or anti_signal:
+            return False
+        stoploss = self.stoploss(index, direction, signal_type)
+        takeprofit = self.takeprofit(index, direction, signal_type)
         current = self.graph.ask_close(index)
         ratio = (takeprofit - current)/(current - stoploss)
-        if signal and ratio > 2.:
+        if signal_type and ratio > 2.:
             self.sl = stoploss
             self.tran_open = current
             return True
